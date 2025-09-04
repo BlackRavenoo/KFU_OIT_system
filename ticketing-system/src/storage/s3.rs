a11572fc50ffc2101b6026438bb0923f1ce1a0cb
Minnakhmetov Almaz <mastera4ek2017@gmail.com>
2025-09-04 15:31:19 +0300

    Add tests for s3 storage

--- ticketing-system/src/storage/s3.rs
+++ ticketing-system/src/storage/s3.rs
@@ -148,3 +148,86 @@ impl FileStorage for S3Storage {
             }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use aws_sdk_s3::{operation::{get_object::GetObjectOutput, head_object::HeadObjectOutput, put_object::PutObjectOutput}, Client};
+    use aws_smithy_mocks::{mock, Rule, RuleMode};
+    use claim::assert_ok;
+
+    use crate::storage::{FileAccess, FileStorage};
+
+    use super::S3Storage;
+
+    fn get_s3_storage(mocks: &[Rule], always_proxy: bool) -> S3Storage {
+        let client = aws_smithy_mocks::mock_client!(
+            aws_sdk_s3,
+            RuleMode::Sequential,
+            mocks
+        );
+
+        S3Storage {
+            client,
+            base_url: "https://example.com".to_string(),
+            always_proxy,
+        }
+    }
+
+    #[tokio::test]
+    async fn get_public_file_access_returns_external_link() {
+        let mock = mock!(Client::head_object)
+            .match_requests(|req| req.bucket() == Some("test-bucket") && req.key() == Some("test-key"))
+            .then_output(|| HeadObjectOutput::builder().build());
+
+        let storage = get_s3_storage(&[mock], false);
+
+        let access = storage.get_file_access("test-bucket", "test-key", true).await.unwrap();
+
+        assert!(matches!(access, FileAccess::ExternalUrl(_)));
+    }
+
+    #[tokio::test]
+    async fn get_private_file_access_returns_external_link() {
+        let head_mock = mock!(Client::head_object)
+            .match_requests(|req| req.bucket() == Some("test-bucket") && req.key() == Some("test-key"))
+            .then_output(|| HeadObjectOutput::builder().build());
+
+        let get_mock = mock!(Client::get_object)
+            .match_requests(|req| req.bucket() == Some("test-bucket") && req.key() == Some("test-key"))
+            .then_output(|| GetObjectOutput::builder().build());
+
+        let storage = get_s3_storage(&[head_mock, get_mock], false);
+
+        let access = storage.get_file_access("test-bucket", "test-key", false).await.unwrap();
+
+        assert!(matches!(access, FileAccess::ExternalUrl(_)));
+    }
+
+    #[tokio::test]
+    async fn get_file_access_returns_stream_if_always_proxy() {
+        let get_mock = mock!(Client::get_object)
+            .match_requests(|req| req.bucket() == Some("test-bucket") && req.key() == Some("test-key"))
+            .then_output(|| GetObjectOutput::builder().build());
+
+        let storage = get_s3_storage(&[get_mock], true);
+
+        let access = storage.get_file_access("test-bucket", "test-key", false).await.unwrap();
+
+        assert!(matches!(access, FileAccess::Stream(_)));
+    }
+
+    #[tokio::test]
+    async fn store_data_correctly_sends_data() {
+        let data = b"test-data";
+
+        let store_mock = mock!(Client::put_object)
+            .match_requests(|req| req.bucket() == Some("test-bucket") && req.key() == Some("test-key") && req.body().bytes() == Some(data))
+            .then_output(|| PutObjectOutput::builder().build());
+
+            let storage = get_s3_storage(&[store_mock], false);
+
+            let res = storage.store("test-bucket", "test-key", data.into()).await;
+
+            assert_ok!(res);
+    }
+}
