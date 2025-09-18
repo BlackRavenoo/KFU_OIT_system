0cbdbb30b0411e455ac3b761a9d99f58e54db9fc
Minnakhmetov Almaz <mastera4ek2017@gmail.com>
2025-09-18 08:15:02 +0300

    Add tests for password

--- ticketing-system/src/domain/password.rs
+++ ticketing-system/src/domain/password.rs
@@ -45,3 +45,58 @@ impl TryFrom<String> for Password {
         Self::parse(value)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Password;
+    use claims::assert_err;
+    use fake::{faker::internet::en, Fake as _};
+    use proptest::{prelude::{any, Strategy}, prop_assert, proptest};
+    use rand::{rngs::StdRng, SeedableRng as _};
+
+    #[test]
+    fn short_password_is_rejected() {
+        let pass = "pass1".to_string();
+        assert_err!(Password::parse(pass));
+    }
+
+    #[test]
+    fn long_password_is_rejected() {
+        let pass = "a1".repeat(33).to_string();
+        assert_err!(Password::parse(pass));
+    }
+
+    #[test]
+    fn password_without_letters_is_rejected() {
+        let pass = "123456789".to_string();
+        assert_err!(Password::parse(pass));
+    }
+
+    #[test]
+    fn password_without_digits_is_rejected() {
+        let pass = "abcabcabc".to_string();
+        assert_err!(Password::parse(pass));
+    }
+
+    #[test]
+    fn password_with_non_ascii_chars_is_rejected() {
+        let pass = "Пароль1312".to_string();
+        assert_err!(Password::parse(pass));
+    }
+
+    fn valid_password_strategy() -> impl Strategy<Value = String> {
+        any::<u64>().prop_map(|seed| {
+            let mut rng = StdRng::seed_from_u64(seed);
+            let mut pass: String = en::Password(7..64).fake_with_rng(&mut rng);
+            pass.push('1');
+            pass
+        })
+    }
+
+    proptest! {
+        #[test]
+        fn valid_emails_are_parsed_successfully(email in valid_password_strategy()) {
+            prop_assert!(Password::parse(email).is_ok());
+        }
+    }
+}
