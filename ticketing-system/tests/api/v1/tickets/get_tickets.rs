b19f2035ae7f0cc58d8e6cef0140b1f861ec0361
Minnakhmetov Almaz <mastera4ek2017@gmail.com>
2025-09-18 08:15:44 +0300

    Add more tests for get tickets route

--- ticketing-system/tests/api/v1/tickets/get_tickets.rs
+++ ticketing-system/tests/api/v1/tickets/get_tickets.rs
@@ -122,5 +122,111 @@ async fn get_tickets_returns_correct_items_count() {
     
         assert_eq!(data.len(), size);
     }
+}
 
+#[tokio::test]
+async fn get_tickets_with_big_filter_returns_tickets() {
+    let app = spawn_app().await;
+
+    let (access, _) = app.get_admin_jwt_tokens().await;
+
+    sqlx::query!(
+        r#"
+            INSERT INTO tickets(building_id, title, author, author_contacts, description)
+            VALUES (1, 'Test title', 'Test author', '999456', 'Test description')
+        "#
+    )
+    .execute(&app.db_pool)
+    .await
+    .unwrap();
+
+    let resp = reqwest::Client::new()
+        .get(format!("{}/v1/tickets/?statuses[]=open&priorities[]=low&order_by=plannedat&sort_order=desc&page=1&page_size=12&buildings[]=1&search=test%20title", app.address))
+        .bearer_auth(access)
+        .send()
+        .await
+        .unwrap();
+
+    let json: serde_json::Value = resp.json().await.unwrap();
+
+    let data = json["items"].as_array().unwrap();
+
+    assert!(!data.is_empty());
+}
+
+#[tokio::test]
+async fn get_tickets_ordered_by_priority_returns_ticket_with_higher_priority_first() {
+    let app = spawn_app().await;
+
+    let (access, _) = app.get_admin_jwt_tokens().await;
+
+    insert_random_tickets(&app.db_pool).await;
+
+    sqlx::query!(
+        r#"
+            INSERT INTO tickets(building_id, title, author, author_contacts, description, priority)
+            VALUES (1, 'Test title', 'Test author', '999456', 'Test description', 3)
+        "#
+    )
+    .execute(&app.db_pool)
+    .await
+    .unwrap();
+
+    let resp = reqwest::Client::new()
+        .get(format!("{}/v1/tickets/?order_by=priority&sort_order=desc", app.address))
+        .bearer_auth(access)
+        .send()
+        .await
+        .unwrap();
+
+    let json: serde_json::Value = resp.json().await.unwrap();
+
+    let data = json["items"].as_array().unwrap();
+
+    assert_eq!(data.first().unwrap()["title"].as_str().unwrap(), "Test title");
+}
+
+#[tokio::test]
+async fn get_tickets_by_assigned_to_works() {
+    let app = spawn_app().await;
+
+    let (access, _) = app.get_admin_jwt_tokens().await;
+
+    insert_random_tickets(&app.db_pool).await;
+
+    let id = sqlx::query!(
+        r#"
+            INSERT INTO tickets(building_id, title, author, author_contacts, description)
+            VALUES (1, 'Test title', 'Test author', '999456', 'Test description')
+            RETURNING id
+        "#
+    )
+    .fetch_one(&app.db_pool)
+    .await
+    .unwrap()
+    .id;
+
+    sqlx::query!(
+        r#"
+            INSERT INTO tickets_users(ticket_id, assigned_to)
+            VALUES($1, 1)
+        "#,
+        id
+    )
+    .execute(&app.db_pool)
+    .await
+    .unwrap();
+
+    let resp = reqwest::Client::new()
+        .get(format!("{}/v1/tickets/?assigned_to=1", app.address))
+        .bearer_auth(access)
+        .send()
+        .await
+        .unwrap();
+
+    let json: serde_json::Value = resp.json().await.unwrap();
+
+    let data = json["items"].as_array().unwrap();
+
+    assert_eq!(data.first().unwrap()["author"], "Test author");
 }
