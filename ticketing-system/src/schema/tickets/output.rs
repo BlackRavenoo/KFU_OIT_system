b4070a29e969d504aa7eda981db1427024304097
Minnakhmetov Almaz <mastera4ek2017@gmail.com>
2025-11-21 21:50:10 +0300

    Fix

--- ticketing-system/src/schema/tickets/output.rs
+++ ticketing-system/src/schema/tickets/output.rs
@@ -1,8 +1,14 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct Building {
     pub id: i16,
     pub code: String,
+    pub name: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Department {
+    pub id: i16,
     pub name: String,
 }
