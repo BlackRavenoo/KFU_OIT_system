77dc49d860ab9ad001d9c92489398d3fa6351ad6
Minnakhmetov Almaz <mastera4ek2017@gmail.com>
2025-11-28 21:13:06 +0300

    Update routes mod file

--- ticketing-system/src/routes/v1/mod.rs
+++ ticketing-system/src/routes/v1/mod.rs
@@ -1,6 +1,6 @@
 use actix_web::web;
 
-use crate::{auth::{middleware::JwtMiddleware, types::UserRole}, routes::v1::{attachments::get_attachment, auth::{change_password, login, me, refresh_token, register, validate_register_token}, buildings::update_building, departments::{create_department, toggle_department_active, update_department}, pages::{create_page, delete_page, get_page, get_page_data, get_pages}, tags::{create_tag, delete_tag, search_tags, update_tag}, tickets::{assign_ticket_to_self, assign_ticket_to_user, create_ticket, delete_ticket, get_consts, get_ticket, get_tickets, unassign_ticket_from_self, unassign_ticket_from_user, update_ticket}, user::{activate_account, change_user_role, change_user_status, deactivate_account, get_users, invite_user, update_avatar, update_user_profile}}};
+use crate::{auth::{middleware::JwtMiddleware, types::UserRole}, routes::v1::{attachments::get_attachment, auth::{change_password, login, me, refresh_token, register, validate_register_token}, buildings::{create_building, toggle_building_active, update_building}, departments::{create_department, toggle_department_active, update_department}, pages::{create_page, delete_page, get_page, get_page_data, get_pages}, tags::{create_tag, delete_tag, search_tags, update_tag}, tickets::{assign_ticket_to_self, assign_ticket_to_user, create_ticket, delete_ticket, get_consts, get_ticket, get_tickets, unassign_ticket_from_self, unassign_ticket_from_user, update_ticket}, user::{activate_account, change_user_role, change_user_status, deactivate_account, get_users, invite_user, update_avatar, update_user_profile}}};
 
 pub mod auth;
 pub mod tickets;
@@ -119,7 +119,9 @@ pub fn config(cfg: &mut web::ServiceConfig) {
             .service(
                 web::scope("/buildings")
                     .wrap(JwtMiddleware::min_role(UserRole::Admin))
+                    .route("/", web::post().to(create_building))
                     .route("/{id}", web::put().to(update_building))
+                    .route("/{id}/toggle_active", web::post().to(toggle_building_active))
             )
     );
 }
